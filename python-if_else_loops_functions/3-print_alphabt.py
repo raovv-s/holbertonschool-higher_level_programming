816f10d8e5777eec9045dc0406cafd27b3edfa99
raovv-s <nadirmaharramov875@gmail.com>
2025-11-16 14:19:00 +0000

    Dsadl

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    if  c == q or c == e :
+   if chr(c) == q or chr(c) == e :
         continue
-   else:
-        print("{}".format(chr(c)), end="")
+   print("{}".format(chr(c)), end="")
