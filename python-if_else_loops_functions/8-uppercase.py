0e9ce00a5337e572e02e81f18a7cf078a7282cfd
raovv-s <nadirmaharramov875@gmail.com>
2025-11-16 14:53:09 +0000

    dasld

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    return ord('A') <= ord(c) <= ord('Z')
+def uppercase(str):
+    for c in str:
+        if 'a' <= c <= 'z':
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
