a9628c9666bb8257c667318e7a2ec96d3edf7a10
raovv-s <nadirmaharramov875@gmail.com>
2025-11-17 16:25:23 +0000

    dslad

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    if(len(sys.argv) = 0):
+    if(len(sys.argv) == 0):
         print(f"{len(sys.argv)} arguments.")
         break
     if(len(sys.argv) > 0):
