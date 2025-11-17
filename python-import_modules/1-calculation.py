393a5b1174e0c4d9c2d94e8aa098b876658185e9
raovv-s <nadirmaharramov875@gmail.com>
2025-11-17 15:47:18 +0000

    dsld

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    from add_0 import add, sum, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sum(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
