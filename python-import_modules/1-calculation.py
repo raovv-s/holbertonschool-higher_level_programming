9d3344739301f838c73f50110d980421bcae30ab
raovv-s <nadirmaharramov875@gmail.com>
2025-11-17 15:55:41 +0000

    dsld

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,9 +4,7 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    def ls():
-        print("{} + {} = {}".format(a, b, add(a, b)))
-        print("{} + {} = {}".format(a, b, sum(a, b)))
-        print("{} + {} = {}".format(a, b, mul(a, b)))
-        print("{} + {} = {}".format(a, b, div(a, b)))
-    ls()
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sum(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
