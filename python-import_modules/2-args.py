b89bdeb81aa17043823ac3038faf435d09a02e63
raovv-s <nadirmaharramov875@gmail.com>
2025-11-17 16:24:18 +0000

    dald

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    if(len(sys.argv) = 0):
+        print(f"{len(sys.argv)} arguments.")
+        break
+    if(len(sys.argv) > 0):
+        print(f"{len(sys.argv)} arguments.")
+    for i in range(1,len(sys.argv)):
+        a = sys.argv[i]
+        print(f"{i}: {a}")
