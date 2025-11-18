2c05491aa17c1acdbc8b97a802afaf1bd3807620
raovv-s <nadirmaharramov875@gmail.com>
2025-11-18 08:52:10 +0000

    dsldalsd

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    list = [int(arg) for arg in sys.argv[1:]]
+    numbers = [int(arg) for arg in sys.argv[1:]]
     s = 0
-    for i in (list):
+    for i in numbers:
         s += i
     print(s)
-
