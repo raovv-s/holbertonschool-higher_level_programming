33b0ade0cc309ed7a630b62ea31b64d0e83f3681
raovv-s <nadirmaharramov875@gmail.com>
2025-11-18 18:49:50 +0000

    dsaldal

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    my_list = [1,2,3,4,5]
+    def print_list_integer(my_list=[]):
+        for i in range(0,len(my_list)):
+            print(my_list[i])
