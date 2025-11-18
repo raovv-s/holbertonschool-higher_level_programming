a77f51019d2a7c5fb8638cec7222242a3f783dd8
raovv-s <nadirmaharramov875@gmail.com>
2025-11-18 08:26:18 +0000

    djlasd

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ if __name__ == '__main__':
     argv = sys.argv[1:]
     c = len(argv)
     if c ==  0:
-        print(f"{c} arguments")
+        print(f"{c} arguments.")
     else:
         if c == 1:
-          print(f"{c} argument")
+          print(f"{c} argument:")
         else:
-            print(f"{c} arguments")
+            print(f"{c} arguments:")
         for i in range(c):
           print(f"{i+1}: {argv[i]}")
