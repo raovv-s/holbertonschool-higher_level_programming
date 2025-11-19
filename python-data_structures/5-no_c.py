b81eb091f0661388e1731c74bb1775ac6b649867
raovv-s <nadirmaharramov875@gmail.com>
2025-11-19 15:09:40 +0000

    jdldkalsk

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 #replace is not allowed 
 def no_c(my_string):
-    d = len(my_string)
-    for i in range(0,d):
-        if my_string[i] == "%c%" or my_string[i] == "%C%":
-            s = my_string[i]
-            my_string.strip(s)
-    print(my_string)
+    stringc = ""
+    for i in my_string:
+        if i != 'c' or i != 'C':
+            stringc += i 
+    print(stringc)
