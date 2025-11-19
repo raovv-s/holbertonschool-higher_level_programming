08a4d9b0f0d4da58df20c0e65f75bb936ce68bcf
raovv-s <nadirmaharramov875@gmail.com>
2025-11-19 15:02:37 +0000

    djsldksal

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,6 +3,7 @@
 def no_c(my_string):
     d = len(my_string)
     for i in range(0,d):
-        if my_string[i] == "c" or my_string[i] == "C":
-            my_string[i] = "" 
+        if my_string[i] == "%c%" or my_string[i] == "%C%":
+            s = my_string[i]
+            my_string.strip(s)
     print(my_string)
