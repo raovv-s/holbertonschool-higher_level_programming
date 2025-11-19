11254d22ae89786cfd14fdb9f2f45157f9728e9b
raovv-s <nadirmaharramov875@gmail.com>
2025-11-19 15:17:12 +0000

    djdldkaldk

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-#replace is not allowed 
-def no_c(my_string):
+    def no_c(my_string):
     stringc = ""
     for i in my_string:
-        if i != 'c' and  i != 'C':
-            stringc += i 
+        if i != 'c' and i != 'C':
+            stringc += i
     return stringc
