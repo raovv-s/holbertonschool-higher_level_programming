54973ac0ff984d995af670ca1edad7b89a5b0b01
raovv-s <nadirmaharramov875@gmail.com>
2025-11-19 14:54:36 +0000

    djsldkl

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+#replace is not allowed 
 def no_c(my_string):
     d = len(my_string)
     for i in range(0,d):
         if my_string[i] == "c" or my_string[i] == "C":
-            my_string.replace("i", "") # replace method is used for removing 
+            my_string[i] = "" #?
     print(my_string)
