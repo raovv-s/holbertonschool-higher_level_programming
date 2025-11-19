14fae18ffede1304e04d1dd0d35a08d761f3ca06
raovv-s <nadirmaharramov875@gmail.com>
2025-11-19 14:52:06 +0000

    djlsadkl

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     d = len(my_string)
     for i in range(0,d):
         if my_string[i] == "c" or my_string[i] == "C":
-            my_string.remove(i)
+            my_string.replace("i", "") # replace method is used for removing 
     print(my_string)
