02d94880844f7834729890c89c9a82ce7a0ef53a
raovv-s <nadirmaharramov875@gmail.com>
2025-11-21 15:52:42 +0000

    dsjld

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a2 = tuple_a[1] if len(tuple_a) > 0 else 0
 
     b1 = tuple_b[0] if len(tuple_a) > 0 else 0
-    b2 = tuple_b[0] if len(tuple_a) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_a) > 0 else 0
 
-    return (a1 + a2 , b1 + b2 )
+    return (a1 + a2 , b1 + b2)
