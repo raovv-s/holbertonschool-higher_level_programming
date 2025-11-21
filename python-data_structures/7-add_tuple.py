10a22f08b8f4ae33a735b7e1dd54691220c48245
raovv-s <nadirmaharramov875@gmail.com>
2025-11-21 15:50:36 +0000

    jlda

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5]
-def print_list_integer(my_list=[]):
-    for i in my_list
-        print("{}".format(my_list[i]))
+tuple_a = (1,1)
+tuple_b = (1,2)
+def add_tuple(tuple_a=(), tuple_b=()):
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0 
+    a2 = tuple_a[1] if len(tuple_a) > 0 else 0
+
+    b1 = tuple_b[0] if len(tuple_a) > 0 else 0
+    b2 = tuple_b[0] if len(tuple_a) > 0 else 0
+
+    return (a1 + a2 , b1 + b2 )
