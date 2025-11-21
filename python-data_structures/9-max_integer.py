38be453c7376dee9fd169a35f61aade4d0217e42
raovv-s <nadirmaharramov875@gmail.com>
2025-11-21 16:28:01 +0000

    djsk

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     c = my_list[0]
-    for i in range(0,len(my_list)):
+    for i in range(0, len(my_list)):
         if my_list[i] > c:
             c = my_list[i]
     return c
