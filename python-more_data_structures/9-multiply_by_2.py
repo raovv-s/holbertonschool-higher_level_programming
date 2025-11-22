c8fc5d53550e0c8bdbc3e87ee463faa98b0445ed
raovv-s <nadirmaharramov875@gmail.com>
2025-11-22 15:43:27 +0000

    sdjskd

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    keys = a_dictionary.keys()
-    values = a_dictionary.values()
-    nvalues = list(map(lambda x: x*2 , values))
-    print("{}: {}".format(keys, nvalues))
-    
+    return {k: v * 2 for k, v in a_dictonary.items()}
