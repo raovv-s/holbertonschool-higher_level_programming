0fa699bc6793711bb4bf53ef8882be857483425e
raovv-s <nadirmaharramov875@gmail.com>
2025-11-22 16:43:10 +0000

    sdl

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
-        return None
+        return 0
     s = 0
     roman_dict = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
     l = len(roman_string)
