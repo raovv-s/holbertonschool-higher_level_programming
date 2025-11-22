53e6717624839f3dd75f1b9ef41fd37c4fb7915f
raovv-s <nadirmaharramov875@gmail.com>
2025-11-22 16:28:05 +0000

    sdjskd

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return None
+    roman_dict = {
+        "I": 1, "V": 5, "X": 10, "L": 50,
+        "C": 100, "D": 500, "M": 1000
+    }
+    for i in range(0, len(roman_string)):
+        d = roman_string[i]
+        e = roman_dict.get(d)
+        print("{} = {}".format(d, e))
