c8e88b23e870941b261c75dd1be83d471c5d6d89
raovv-s <nadirmaharramov875@gmail.com>
2025-11-22 18:27:56 +0000

    sdjks

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
             print("{}".format(my_list[i], end=""))
-            count += 1 
+            s += 1 
     except IndexError:
         pass
     print()
 
-    return count
+    return s
