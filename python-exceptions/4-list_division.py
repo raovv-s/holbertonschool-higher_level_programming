26ce7d87b77d68d9d82f482f55323e1851132444
raovv-s <nadirmaharramov875@gmail.com>
2025-11-22 19:43:07 +0000

    sdk

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -16,4 +16,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             s = 0
         finally:
-            return new_list
+            new_list.append(s)
+    return new_list
+    
