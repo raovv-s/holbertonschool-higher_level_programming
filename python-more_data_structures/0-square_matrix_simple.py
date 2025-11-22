c1f1cd50a0a5bb177d28588cef0758abf7d181a0
raovv-s <nadirmaharramov875@gmail.com>
2025-11-22 13:27:34 +0000

    dsdjlad

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-# I used map twice because we need to access each element like 2 for loops but with map
-    squared_matrix = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    # I used map twice because we need to access each element
+    # like 2 for loops but with map
+    squared_matrix = list(
+        map(lambda row: list(map(lambda x: x**2, row)), matrix)
+    )
     return squared_matrix
