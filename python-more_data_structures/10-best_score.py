6eeec45ec69dcfd92dbe3242f08786f1b5e0940a
raovv-s <nadirmaharramov875@gmail.com>
2025-11-22 15:56:13 +0000

    sdjskd

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    values = []
+    keys = []
+    for k, v in a_dictionary.items():
+        keys.append(k)
+        values.append(v)
+    mvalue = max(values)
+    max_index = values.index(mvalue)
+    return keys[max_index]
     
