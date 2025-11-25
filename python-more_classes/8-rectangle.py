2475522b2d2237455e4eb764c371402b4615338d
raovv-s <nadirmaharramov875@gmailcom>
2025-11-25 20:50:05 +0000

    25nov

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -78,7 +78,6 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
-
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """ static method which compares 2 objects"""
