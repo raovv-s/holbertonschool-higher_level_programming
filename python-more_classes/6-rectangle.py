5497a27465a4239f582956462201a7b029c68f25
raovv-s <nadirmaharramov875@gmailcom>
2025-11-25 20:33:57 +0000

    25nov

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -10,12 +10,12 @@ class Rectangle:
     """Rectangle class with private width attribute."""
 
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """Initialize Rectangle with width and height."""
         self.width = width
         self.height = height
-        number_of_instances += 1 
+        Rectangle.number_of_instances += 1 
 
     @property
     def width(self):
@@ -74,5 +74,5 @@ class Rectangle:
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
