2a2faa0e2d19ad2886fc741eef63a053446a5f4f
raovv-s <nadirmaharramov875@gmailcom>
2025-11-25 20:46:46 +0000

    25nov

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -77,6 +77,9 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
 
         if not isinstance(rect_1, Rectangle):
