de6a140e5a29ab0876738a98aa2023c2b70e54ed
raovv-s <nadirmaharramov875@gmailcom>
2025-11-25 20:13:53 +0000

    25nov

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -66,5 +66,6 @@ class Rectangle:
             if i != self.__height - 1:
                 rect += "\n"
         return rect
+
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
