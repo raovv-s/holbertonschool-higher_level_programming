494b3882be3803eec903987ec53c04155bf80c20
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 12:09:22 +0000

    27nov

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function which returns if the objects is instance or not"""
+
+
+def is_same_class(obj, a_class):
+    """function is_same_class"""
+    return type(obj) is a_class
