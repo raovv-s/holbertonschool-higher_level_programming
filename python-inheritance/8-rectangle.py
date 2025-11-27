ab5b8843b6996bde7fa83a87616334a905bd5a6e
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 15:27:14 +0000

    27nov

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+
+
+class Rectangle(BaseGeometry):
+    """init method"""
+
+    def __init__(self, width, height):
+        self.width == __width
+        self.height == __height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        
