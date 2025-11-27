81ce9e5cffded366cfa336a283b2d3fa71163859
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 15:52:00 +0000

    27nov

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -24,6 +24,8 @@ class Rectangle(BaseGeometry):
     def str(self, width, height):
         return(f"[Rectangle] {__width}/{__height}")
     
+    """print method"""
+    
     @property
     def print(self, width, height):
         print(f"[Rectangle] {__width}/{__height}")
