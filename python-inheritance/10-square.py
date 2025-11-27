49d274c95a40982f8a8af69c490eb7dd2d8d1b8e
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 16:12:45 +0000

    27nov

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square Class"""
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+
+        self.__size = size
+        self.integer_validator("size", size)
+    
+    def area(self):
+        """area of square"""
+
+        return self.__size * self.__size
+    def __str__(self):
+        """String representation of rectangle"""
+        return f"[Rectangle] {self.__size}/{self.__size}"
