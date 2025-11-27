16dfee6ed392fe8064460d2d9f0bc2bead56f56e
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 14:29:42 +0000

    27nov

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""exception in area method"""
+
+
+class BaseGeometry:
+    """exception raise"""
+
+    def area(self):
+        raise NotImplementedError("area() is not implemented")
