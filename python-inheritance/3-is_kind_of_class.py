538439f099c4bc7991982b4a36c0b496de7588b7
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 12:15:32 +0000

    27nov

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3 
+"""function with isinstance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function is_kind_of_class, isintance is used in this function"""
+    
+    return isinstance(obj, a_class)
