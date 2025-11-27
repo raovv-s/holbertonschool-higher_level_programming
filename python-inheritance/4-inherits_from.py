e5fb4f8e19914fbe0740e691b2b2ef3f0ac763e3
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 14:21:30 +0000

    27nov

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,5 +5,5 @@
 def inherits_from(obj, a_class):
     """issubclass"""
 
-    return isinstance(obj, a_class) and type(obj) is a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
 
