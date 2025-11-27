bbbd0df8b7cda892c49f3e4bc4255fbe60dd5800
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 16:19:59 +0000

    27nov

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -18,4 +18,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """String representation of rectangle"""
-        return f"[Rectangle] {self.__size}/{self.__size}"
+        return f"[Square] {self.__size}/{self.__size}"
