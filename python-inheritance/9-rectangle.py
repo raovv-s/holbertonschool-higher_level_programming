921ec0e17579ebb629b983cd1881cb73c6c0c787
raovv-s <nadirmaharramov875@gmail.com>
2025-11-27 15:51:26 +0000

    27nov

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """init method"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+    
+    """area method"""
+
+    @property
+    def area(self, width, height):
+        return __width * __height
+    
+    """print method"""
+
+    @property
+    def str(self, width, height):
+        return(f"[Rectangle] {__width}/{__height}")
+    
+    @property
+    def print(self, width, height):
+        print(f"[Rectangle] {__width}/{__height}")
