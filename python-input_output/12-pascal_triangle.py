d0084bd99812f37b5ba9eef5f8dd53203286b35b
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 20:41:45 +0000

    29nov

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Pascal Triangle"""
+
+
+def pascal_triangle(n):
+    """triangle function"""
+    
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1] * (i + 1)
+
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+        triangle.append(row)
+
+    return triangle
