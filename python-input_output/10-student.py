e3f92545b9a07bf81dd8053e2f61252929f824b5
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 17:29:26 +0000

    28nov

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Student Class with json function"""
+
+
+class Student:
+    """Class Student with init and to_json function"""
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """to json"""
+
+        if isinstance(attrs, list):
+
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+
+        return self.__dict__
