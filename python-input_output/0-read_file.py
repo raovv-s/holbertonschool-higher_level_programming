aebc54303322d15a927f6251ebcc1ff5f8b22b8c
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 15:15:47 +0000

    28nov

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""READ UTF-8"""
+
+
+def read_file(filename=""):
+    """open with using "with" """
+
+    with open("filename", "r", encoding="utf-8") as m:
+        s = m.read()
+        print(s)
+
