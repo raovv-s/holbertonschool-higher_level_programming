8980638e5f0f8d1fd9d28412130de559159c4638
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 16:02:01 +0000

    28nov

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ def save_to_json_file(my_obj, filename):
     """function where it happens"""
     d = json.dumps(my_obj) #json
     with open(filename, "w", encoding="utf-8") as m:
-        f.write(d)
+        m.write(d)
 
