14a7ac5c00ee9e7c35169a2f5d3a33a6340c7182
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 17:20:27 +0000

    28nov

---
+++ python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Student Class with json function"""
+
+class Student:
+    """Class Student with init and to_json function"""
+
+    def __init__(self, first_name, last_name, age):
+        """init"""
+
+        self.first_name = first_name
+        self.last_name = last_name 
+        self.age = age
+    
+    def to_json(self):
+        return self.__dict__
