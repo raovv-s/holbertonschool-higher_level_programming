ee85d47435c4ffe53e90c20855c135b12d11caf8
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 17:01:38 +0000

    28nov

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,8 @@
 save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
 load_from_json_file = __import__(6-load_from_json_file).load_from_json_file
 
+filename = "add_item.json"
+
 try:
     obj = load_from_json_file(filename)
 except FileNotFoundError:
