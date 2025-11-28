d350f68dded0a1faf27ee77145e2cf813dbbb3ed
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 16:59:36 +0000

    28nov

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,11 +2,12 @@
 """load add save"""
 
 
-import json
 save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
 load_from_json_file = __import__(6-load_from_json_file).load_from_json_file
+
 try:
     obj = load_from_json_file(filename)
 except FileNotFoundError:
     obj = []
+
 save_to_json_file(obj, filename)
