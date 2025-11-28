df4e54ef861363de9a90f6eb4359c37292995ef0
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 15:36:13 +0000

    28nov

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """write_file function"""
     with open(filename, "w", encoding="utf-8") as m:
-        d = text 
+        d = text
         return m.write(d)
