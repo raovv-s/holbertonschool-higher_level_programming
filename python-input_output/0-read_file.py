97532e6623f444d948bf85ce93c8b2792408f0d8
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 15:18:20 +0000

    28nov

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,5 @@
 def read_file(filename=""):
     """open with using "with" """
 
-    with open("filename.txt", "r", encoding="utf-8") as m:
-        s = m.read()
-        print(s)
-
+    with open(filename, "r", encoding="utf-8") as m:
+        print(m.read(), end="")
