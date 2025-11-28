188badf66fb93a98af48053ba514748aa624b6a1
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 17:02:41 +0000

    28nov

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """load add save"""
 
 
-save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
-load_from_json_file = __import__(6-load_from_json_file).load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
