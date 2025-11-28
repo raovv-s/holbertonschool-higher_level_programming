01a3d66c30af27f96efa3ec3222d6608bba96b10
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 17:03:45 +0000

    28nov

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """load add save"""
 
-
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -11,5 +11,7 @@ try:
     obj = load_from_json_file(filename)
 except FileNotFoundError:
     obj = []
+
+items.extend(argv[1:])
 
 save_to_json_file(obj, filename)
