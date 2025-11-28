f784828c471bbbe9e3a3b56280df6c0423eea9f8
raovv-s <nadirmaharramov875@gmail.com>
2025-11-28 17:34:57 +0000

    28nov

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -12,7 +12,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """to json"""
 
         if isinstance(attrs, list):
