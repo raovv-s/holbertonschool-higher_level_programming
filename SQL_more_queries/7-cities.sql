b719e35d4647d944edbf2047ae7dae1c969a4a4b
raovv-s <nadirmaharramov875@gmail.com>
2025-11-29 21:03:11 +0000

    30nov

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- FOREIGN KEY
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256),
+    state_id INT NOT NULL, 
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
