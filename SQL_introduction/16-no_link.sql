895810c0968ec51b648221829fde0f96538bb53d
raovv-s <nadirmaharramov875@gmail.com>
2025-11-29 17:08:45 +0000

    29nov

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Task 16 
-SELECT score, name FROM second_table WHERE name != "" or IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name != "" and name IS NOT NULL ORDER BY score DESC;
