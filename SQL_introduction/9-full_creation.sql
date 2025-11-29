36ca76e706c616cf66a242d616cf5dead06f7862
raovv-s <nadirmaharramov875@gmail.com>
2025-11-29 13:11:39 +0000

    29nov'

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,11 @@
 -- create and insert values 
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT
-    name VARCHAR(256)
+    id INT,
+    name VARCHAR(256),
     score INT
-)
-INSERT INTO second_table(id, name, score) VALUES(
-    1, “John”, 10
-    2, “Alex”, 3
-    3, “Bob”, 14
-    4, “George”, 8
-)
+);
+INSERT INTO second_table(id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
