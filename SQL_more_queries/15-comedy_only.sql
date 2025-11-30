79baf0fa65df1fcc8c961314a94b24b88b0b6793
raovv-s <nadirmaharramov875@gmail.com>
2025-11-30 09:59:54 +0000

    30nov

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,6 @@
 SELECT tv_genres.name 
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_shows_genres.genre_id = tv_genres.id
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC 
