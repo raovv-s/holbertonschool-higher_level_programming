c966ccde004c736f3a09b4d5b1e68c88bdc1d855
raovv-s <nadirmaharramov875@gmail.com>
2025-11-30 09:20:32 +0000

    30nov

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- task 10
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows
+FROM tvshows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
