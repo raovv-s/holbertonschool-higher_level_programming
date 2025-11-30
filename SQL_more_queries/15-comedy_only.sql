6a051d1b320bb5be3f9706f918aa47482cd82c4c
raovv-s <nadirmaharramov875@gmail.com>
2025-11-30 10:00:58 +0000

    30nov

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- task 15
-SELECT tv_genres.name 
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
