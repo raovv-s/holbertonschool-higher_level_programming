c8b3cc3724fc9d257952b37e9ec0c35f1a54bfd9
raovv-s <nadirmaharramov875@gmail.com>
2025-12-01 21:02:29 +0000

    2dec

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status")
     print("Body response")
-    print(f"type: {type(req.text)}")
-    print(f"content: {req.text}")
+    print(f"\t- type: {type(req.text)}")
+    print(f"\t- content: {req.text}")
