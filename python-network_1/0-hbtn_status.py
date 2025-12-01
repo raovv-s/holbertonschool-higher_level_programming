42875cadc39c5f8971f8cac9817d0d05e14b960c
raovv-s <nadirmaharramov875@gmail.com>
2025-12-01 20:03:29 +0000

    2dec

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as f:
+        request = f.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(request)))
+    print("\t- content: {}".format(request))
+    print("\t- utf8 content: {}".format(request.decode('utf-8')))
