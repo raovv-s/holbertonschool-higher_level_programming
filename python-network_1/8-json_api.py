8ad60284f362906fae8d9e8d5e59a34987daa489
raovv-s <nadirmaharramov875@gmail.com>
2025-12-02 10:12:55 +0000

    2dec

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -2,7 +2,7 @@
 """post json data via using requests lib"""
 import requests 
 import sys
-if __name__ == "__main__"":
+if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     q = sys.argv[1] if len(sys.argv) > 1 else ""
     parse = requests.post(url, data={"q": q})
