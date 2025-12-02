85f2d69bb466b8ef8878c2993e004f08d92993ac
raovv-s <nadirmaharramov875@gmail.com>
2025-12-02 16:38:34 +0000

    2dec

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-"""jsonplaceholder api"""
+"""jsonapiholder api example"""
+
 import requests
+import csv
 
+URL = "https://jsonplaceholder.typicode.com/posts"
 
-url = "https://jsonplaceholder.typicode.com/posts"
-req = requests.get(url)
-
-def fetch_and_print_posts(req):
-    """post dump from JSONPlaceHolder"""
-    req = requests.get(url)
-    print(f"Status code: {req.status_code}")
+def fetch_and_print_posts():
+    """Fetch posts and print their titles"""
+    req = requests.get(URL)
+    print(f"Status Code: {req.status_code}")
+    
     if req.status_code == 200:
-        json = req.json()
+        posts = req.json()
         for post in posts:
             print(post["title"])
     else:
-        print("None")
+        print("Ошибка запроса")
 
-def fetch_and_save_posts(req):
-    req = requests.get(url)
-
-    if req.status_code == 200:
-        json = req.json()
-        keydata = []
-        for key in json:
-            keydata.append({
-            "id": post["id"],
-            "title": post["title"],
-            "body": post["body"]
-                           })
+def fetch_and_save_posts():
+    """Fetch posts and save them to a CSV file"""
+    req = requests.get(URL)
     
-    with open("posts.csv", "w", encoding="utf-8") as f:
-        writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
-        writer.writeheader()
-        writer.writerows(structured_posts)
+    if req.status_code == 200:
+        posts = req.json()
+        structured_posts = []
+
+        for post in posts:
+            structured_posts.append({
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            })
+        
+        with open("posts.csv", "w", newline='', encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(structured_posts)
