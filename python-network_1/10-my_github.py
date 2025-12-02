f307b52c2093a10c2b377e29e03c29746d5aa7b4
raovv-s <nadirmaharramov875@gmail.com>
2025-12-02 15:45:13 +0000

    2dec

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" basic auth via github api"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, password))
+
+    try:
+        r = response.json()
+    except ValueError:
+        return None
+    finally:
+        id = r.get("id")
