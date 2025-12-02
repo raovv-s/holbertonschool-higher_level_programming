a34fcfea05d490bacf5e0be8d82f8a59f713fb6b
raovv-s <nadirmaharramov875@gmail.com>
2025-12-02 09:15:46 +0000

    2dec

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -2,7 +2,7 @@
 """sys + requests"""
 import sys
 import requests
-if __name __ == "__main__":
+if __name__ == "__main__":
     url = sys.argv[1]
     getr = requests.get(url)
     print(getr.header("X-Request-Id"))
