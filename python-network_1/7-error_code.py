88a32e51c4a0f45e8c8199f9e7b0ab2b804e3465
raovv-s <nadirmaharramov875@gmail.com>
2025-12-02 09:51:02 +0000

    2dec

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""request error"""
+import sys 
+import requests
+
+if __name__ = "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    StatusCode = req.status_code
+    if StatusCode >= 400:
+        print(f"Error code: {StatusCode}")
