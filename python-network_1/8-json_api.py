845879472c6b6581f56b979b293ff5ccd24c4a69
raovv-s <nadirmaharramov875@gmail.com>
2025-12-02 15:37:28 +0000

    2dec

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """post json data via using requests lib"""
-import requests 
+import requests
 import sys
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    
+
     q = sys.argv[1] if len(sys.argv) > 1 else ""
-    
+
     parse = requests.post(url, data={"q": q})
-    
+
     try:
         r = parse.json()
     except ValueError:
-        print("Not a valid JSON")    
+        print("Not a valid JSON") 
     if not r:
         print("No result")
     else:
         print("[{}] {}".format(r.get("id"), r.get("name")))
-
